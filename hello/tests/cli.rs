e308e3fcbbffca5fee078550dd67afdf84833360
Takeru Hayasaka <hayatake396@gmail.com>
2024-04-14 16:34:55 +0900

    cli 1 chapter

---
+++ hello/tests/cli.rs
@@ -0,0 +1,27 @@
+use assert_cmd::Command;
+
+#[test]
+fn works() {
+    assert!(true);
+}
+
+#[test]
+fn runs() {
+    // letは定数である
+    // letは変数を作成する
+    // mutは変数の値を変更できるようにする
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success().stdout("Hello, world!\n");
+}
+
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
+
+#[test]
+fn false_not_ok(){
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}
